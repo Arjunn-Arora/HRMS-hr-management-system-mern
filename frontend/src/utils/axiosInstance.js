33a0a6ea5f79353b0e545f846308216671995036
Arjun Arora <06arjunarora@gmail.com>
2025-08-16 19:32:52 +0530

    fix: update axios base URL to point to the production server

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/api", // your backend base
+  baseURL: "https://hrms-hr-management-system-mern.onrender.com/api", // your backend base
   withCredentials: true, // for cookie access
 });
 
