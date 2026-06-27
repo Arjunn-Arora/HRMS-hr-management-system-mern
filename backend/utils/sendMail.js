7a206f66b12a547d6ba8fb3ef1f097bd83a13f18
Arjun Arora <06arjunarora@gmail.com>
2026-06-27 19:39:35 +0530

    debug

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,24 +1,39 @@
 import axios from "axios";
 
 const sendMail = async (to, subject, html) => {
-  await axios.post(
-    "https://api.brevo.com/v3/smtp/email",
-    {
-      sender: {
-        name: "HRMS",
-        email: process.env.SENDER_EMAIL,
+  try {
+    const response = await axios.post(
+      "https://api.brevo.com/v3/smtp/email",
+      {
+        sender: {
+          name: "HRMS",
+          email: process.env.SENDER_EMAIL,
+        },
+        to: [
+          {
+            email: to,
+          },
+        ],
+        subject: subject,
+        htmlContent: html,
       },
-      to: [{ email: to }],
-      subject,
-      htmlContent: html,
-    },
-    {
-      headers: {
-        "api-key": process.env.BREVO_API_KEY,
-        "Content-Type": "application/json",
-      },
-    }
-  );
+      {
+        headers: {
+          "api-key": process.env.BREVO_API_KEY,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    );
+
+    console.log("Mail sent:", response.data);
+    return response.data;
+  } catch (err) {
+    console.error("Brevo Error:");
+    console.error("Status:", err.response?.status);
+    console.error("Response:", err.response?.data);
+    throw err;
+  }
 };
 
 export default sendMail;
