b998691fe043de7881d765d47400373a6cb999c9
Arjun Arora <06arjunarora@gmail.com>
2026-06-27 19:08:59 +0530

    configured sendMail

--- backend/utils/sendMail.js
+++ backend/utils/sendMail.js
@@ -1,10 +1,26 @@
-import transporter from "./nodemailer.js";
-const sendMail = async (to, subject, html) => {
-  await transporter.sendMail({
-    from: `HRMS <${process.env.SENDER_EMAIL}>`,
-    to,
-    subject,
-    html,
-  });
+import axios from "axios";
+
+export const sendMail = async (to, subject, html) => {
+  await axios.post(
+    "https://api.brevo.com/v3/smtp/email",
+    {
+      sender: {
+        name: "HRMS",
+        email: process.env.SENDER_EMAIL,
+      },
+      to: [
+        {
+          email: to,
+        },
+      ],
+      subject,
+      htmlContent: html,
+    },
+    {
+      headers: {
+        "api-key": process.env.BREVO_API_KEY,
+        "Content-Type": "application/json",
+      },
+    }
+  );
 };
-export default sendMail;
